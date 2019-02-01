e3d7577774f13659014e3bed643797915b441e01
Aida Ospanova <ospanovaida08@gmail.com>
2019-02-01 12:38:51 +0600

    Added new tast class for testing PatientDAO

--- src/test/scala/PatientTest.scala
+++ src/test/scala/PatientTest.scala
@@ -1,4 +1,4 @@
-import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec, WordSpecLike}
+import org.scalatest._
 import akka.http.scaladsl.model.{MessageEntity, StatusCodes}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import akka.http.scaladsl.server._
@@ -13,125 +13,75 @@ import dao.PatientDAO
 import model.{FullPatient, Patient}
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
-//class PatientTest extends FunSuite {
-//    val patientDao = new PatientDAO()
-//    test("Get_Patient_by_7"){
-//        val p: Future[Seq[Patient]] = patientDao.getPatient(7)
-//        var ok = false
-//        p.onComplete{
-//            case Success(value) => {
-//                if (value.head.equals(Patient(Some(7),"Dikosh", "Ospan", "asdf", "asdfg"))) {
-//                    ok = true
-//                }
-//                assert(ok)
-//            }
-//            case Failure(exp) => println(exp.getMessage)
-//        }
-//    }
-//    test("Update_Patient") {
-//        val oldP = patientDao.getPatient(7);
-//        val p = patientDao.updatePatient(7, Patient(Some(7), "Kura", "Ospan", "asdf", "asdfg"))
-//        var ok = false
-//        p.onComplete {
-//            case Success(value) => {
-//                oldP.onComplete {
-//                    case Success(value) => {
-//                        val c = patientDao.updatePatient(7, value.head)
-//                        c.onComplete{
-//                            case Success(value) => {
-//                                ok = true
-//                                assert(ok)
-//                            }
-//                            case Failure(exp) => println("ERROR")
-//                        }
-//                        //.ready(c, 5.seconds)
-//                    }
-//                    case Failure(exp) => println(exp.getMessage)
-//                }
-//            }
-//            case Failure(exp) => {
-//                println("UPDATE_ERROR")
-//            }
-//        }
-//
-//    }
-//    test("Delete_Patient") {
-//        val oldP = patientDao.getPatient(7)
-//        val cur = patientDao.deletePatient(7)
-//        var ok = false
-//        cur.onComplete {
-//            case Success(value) => {
-//                oldP.onComplete {
-//                    case Success(p) => {
-//                        val fP = FullPatient(p.head.name, p.head.surname, p.head.login, p.head.password)
-//                        val c = patientDao.addPatient(fP)
-//                        //Await.ready(c, 5.seconds)
-//                        ok = true
-//                        assert(ok)
-//                    }
-//                    case Failure(exp) => {
-//                        ok = false
-//                        println(exp.getMessage)
-//                    }
-//                }
-//            }
-//            case Failure(exp) => println(exp.getMessage)
-//        }
-//
-//    }
-
-//}
-
-class AkkaTest(_system: ActorSystem)  extends TestKit(_system)
-  with DefaultTimeout with ImplicitSender
-  with WordSpecLike with Matchers with BeforeAndAfterAll {
-    import PatientActor._
-    def this() = this(ActorSystem("AkkaTestSpec"))
-    val pActor = system.actorOf(PatientActor.props, "PatientActor")
-    override def afterAll {
-        shutdown()
+class PatientTest extends FunSuite {
+    val patientDao = new PatientDAO()
+    test("Get_Patient_by_7"){
+        val p: Future[Seq[Patient]] = patientDao.getPatient(7)
+        var ok = false
+        p.onComplete{
+            case Success(value) => {
+                if (value.head.equals(Patient(Some(7),"Dikosh", "Ospan", "asdf", "asdfg"))) {
+                    ok = true
+                }
+                assert(ok)
+            }
+            case Failure(exp) => println(exp.getMessage)
+        }
     }
-
-    "An ConnectionActor" should {
-        val patientDao = new PatientDAO()
-        "return patients in getting request" in {
-            within(500 millis) {
-                pActor ! AddPatient(FullPatient("Ulzhan", "Ospan", "asd", "asdf"))
-                expectMsg(ActionPerformed("Added"))
-            }
-        }
-        "delete patient " in {
-            within(500 millis) {
-                pActor ! DeletePatient(23)
-                expectMsg(ActionPerformed("Deleted"))
-            }
-        }
-        "doesn't delete patient " in {
-            within(500 millis) {
-                pActor ! DeletePatient(111)
-                expectMsg(ActionPerformed("Failed"))
-            }
-        }
-        "get patient by ID " in {
-            within(500 millis) {
-
-            }
-        }
-        "update patient " in  {
-            within(500 millis) {
-                val oldP = patientDao.getPatient(8);
-                pActor ! UpdatePatient(12, Patient(Some(8), "Dikosh", "Murzekenova", "dimur", "12345"))
+    test("Update_Patient") {
+        val oldP = patientDao.getPatient(7);
+        val p = patientDao.updatePatient(7, Patient(Some(7), "Kura", "Ospan", "asdf", "asdfg"))
+        var ok = false
+        p.onComplete {
+            case Success(value) => {
                 oldP.onComplete {
                     case Success(value) => {
-                        pActor ! UpdatePatient(8, value.head)
+                        val c = patientDao.updatePatient(7, value.head)
+                        c.onComplete{
+                            case Success(value) => {
+                                ok = true
+                                assert(ok)
+                            }
+                            case Failure(exp) => println("ERROR")
+                        }
+                        //.ready(c, 5.seconds)
                     }
-                    case Failure(exception) => println(exception.getMessage)
+                    case Failure(exp) => println(exp.getMessage)
                 }
-                expectMsg(ActionPerformed("Failed"))
+            }
+            case Failure(exp) => {
+                println("UPDATE_ERROR")
             }
         }
 
     }
+    test("Delete_Patient") {
+        val oldP = patientDao.getPatient(7)
+        val cur = patientDao.deletePatient(7)
+        var ok = false
+        cur.onComplete {
+            case Success(value) => {
+                oldP.onComplete {
+                    case Success(p) => {
+                        val fP = FullPatient(p.head.name, p.head.surname, p.head.login, p.head.password)
+                        val c = patientDao.addPatient(fP)
+                        //Await.ready(c, 5.seconds)
+                        ok = true
+                        assert(ok)
+                    }
+                    case Failure(exp) => {
+                        ok = false
+                        println(exp.getMessage)
+                    }
+                }
+            }
+            case Failure(exp) => println(exp.getMessage)
+        }
+
+    }
+
 }
+
